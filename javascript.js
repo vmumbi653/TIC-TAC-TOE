17732870d5218d3dd8d92c5ef98a25e3c6f937ba
vmumbi653 <[mainamumbi23@gmail.com]>
2025-05-07 10:44:22 +0300

    CREATE GAMEFLOW FUNCTION
    I have created the gameFlow function that controls the flow of the game. In this function there is the createPlayer function that returns a player object, and also a playGame function that shows the flow of the game.

--- javascript.js
+++ javascript.js
@@ -1,35 +1,81 @@
 const game = (function() {
     const gameBoard = [];   //store gameboard in array
-    let row = 3;
-    let column = 3;
+    // let row = 3;
+    // let column = 3;
+    
     
     //function to create gameboard
     function createBoard() {
-        for(let i = 0; i< row; i++) {
-            gameBoard[i] = [];
-            for(let j = 0; j<column; j++) {
-                gameBoard[i].push(cell());
-            }
-        }
+       gameBoard = new Array(9).fill("");
     }
+
+    //function to get cell index and its value
+    function getCell(index, value) {
+        gameBoard[index] = value;
+    };
 
     //function to render board
-    const showBoard = () => gameBoard;
+    const showBoard = () => [...gameBoard];
 
-    //function to createPlayer
-    function createPlayer(name, marker) {
-        return {
-            name: name,
-            marker: marker
+    //function to reset board
+    const resetBoard = () => {
+        gameBoard = ["", "", "", "", "", "", "", "", "",];
+    }
+
+
+    return{createBoard, showBoard, getCell, resetBoard};
+})();
+
+
+//function to direct flow of the game
+function gameFlow() {
+    let isPlayerOnesTurn = true;
+     //function to createPlayer
+ const createPlayer = (name, marker) => {
+    return {
+        name: name,
+        marker: marker
+    }
+
+};  
+    // //render board
+    // showBoard();
+
+    // let playerOne = createPlayer("jimmy", "X");
+    // let playerTwo = createPlayer("jonny", "O");
+
+    const playGame = () => {
+        if(isPlayerOnesTurn === true) {
+            isPlayerOnesTurn = false;
+            console.log(`${playerOne.marker}`);
+            console.log(`${playerOne.name}`);
+        } else {
+            isPlayerOnesTurn = true;
+            console.log(`${playerTwo.marker}`);
+            console.log(`${playerTwo.name}`);
         }
     }
 
-    return{createBoard, showBoard, createPlayer};
-})();
-
+    return {createPlayer, playGame};
+}
 console.log(game.createBoard);
 console.log(game.showBoard);
-const player1 = game.createPlayer("jane", "X");
-const player2 = game.createPlayer("josh", "O");
-console.log(player1);
-console.log(player2);
+
+const startGame = gameFlow();
+console.log(startGame.playGame);
+console.log(startGame.createPlayer("jane", "X"));
+console.log(startGame.createPlayer("evie", "0"));
+
+const playerOne = startGame.createPlayer("jane", "X");
+const playerTwo = startGame.createPlayer("evie", "O");
+console.log(playerOne);
+console.log(playerTwo);
+
+console.log(startGame.playGame(playerOne));
+console.log(startGame.playGame(playerTwo));
+
+
+// const player1 = createPlayer("jane", "X");
+// const player2 = createPlayer("josh", "O");
+// console.log(player1);
+// console.log(player2);
